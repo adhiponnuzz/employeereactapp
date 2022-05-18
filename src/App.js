7c7c9b1a5466fbe2d1283156e3b77284da81a9fa
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-05-18 23:00:15 +0530

    view finish

--- src/App.js
+++ src/App.js
@@ -4,12 +4,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Empregister from './component/Empregister';
 import Search from './component/Search';
+import Navbar from './component/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import View from './component/View';
 function App() {
   return (
     <>
     
-    <Empregister/>
-    <Search/>
+    <BrowserRouter>
+    <Routes>
+
+      <Route path="/" exact element={<Empregister/>}/>
+      <Route path="/search" exact element={<Search/>}/>
+      <Route path="/view" exact element={<View/>}/>
+
+      
+
+
+
+
+    </Routes>
+    
+    
+    
+    
+    
+    
+    </BrowserRouter>
     
     </>
     
